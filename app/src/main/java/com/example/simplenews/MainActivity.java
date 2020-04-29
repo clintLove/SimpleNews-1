896053979ff2e6bff50426abc7a2f81b09b334ae
cliffordli <1833015726@qq.com>
2020-04-29 20:03:38 +0800

    侧边栏实现及响应添加，图标添加

--- app/src/main/java/com/example/simplenews/MainActivity.java
+++ app/src/main/java/com/example/simplenews/MainActivity.java
@@ -1,14 +1,102 @@
 package com.example.simplenews;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;//弹窗导入包
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*顶部栏*/
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+        //侧边栏抽屉
+        DrawerLayout drawer=findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        //drawer.addDrawerListener();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //按返回键时的作用
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {//左侧边栏是否打开
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.item_favorite) {
+            // Handle the subscribe action
+            popout("favorite","你正在访问favorite","");
+            //TODO: goto favorite
+        } else if (id == R.id.item_history) {
+            Intent intent = new Intent();
+            popout("history","你正在访问history","");
+
+            //TODO: goto history
+        } else if (id == R.id.item_setting) {
+            Intent intent = new Intent();
+            popout("setting","你正在访问favorite","");
+            //TODO: goto setting
+        } else if (id == R.id.subitem_01) {
+            Intent intent = new Intent();
+            popout("subitem","你正在访问subitem","");
+            //TODO: DO NOTHING
+        } else if (id == R.id.subitem_02) {
+            popout("subitem","你正在访问subitem","");
+            //TODO: DO NOTHING
+        } else if (id == R.id.subitem_03) {
+            popout("subitem","你正在访问subitem","");
+            //TODO: DONOTHING
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void popout(String title,String message,String button){
+        new AlertDialog.Builder(
+                MainActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("确定", null)
+                .show();
     }
 }
